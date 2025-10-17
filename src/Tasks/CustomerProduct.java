0d24e3071fd01cfb70a3390726b48a2c9ee6516a
Mohamed Walid <mohamedwalid.elkholy@gmail.com>
2025-10-18 01:19:32 +0300

    Final look

--- src/Tasks/CustomerProduct.java
+++ src/Tasks/CustomerProduct.java
@@ -1,9 +1,51 @@
 package Tasks;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CustomerProduct implements RecordsInterface{
 
+private String customerSSN,productID;
+private LocalDate purchaseDate;
+private boolean paid;
+
+public String getCustomerSSN() { 
+    return customerSSN;
 }
- 
 
 
+public String getProductID() { 
+    return productID;
+}
+
+public LocalDate getPurchaseDate() { 
+    return purchaseDate;
+}
+
+public boolean isPaid() { 
+   return paid;
+}
+
+public void setPaid(boolean paid) { 
+    this.paid = paid;
+}
+
+
+public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+    this.customerSSN=customerSSN;
+    this.productID=productID;
+    this.purchaseDate=purchaseDate;
+    this.paid=false;
+}
+
+public String lineRepresentation() {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("day-month-year");
+      return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+}
+public String getSearchKey() {
+     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("day-month-year");
+     return customerSSN + "," + productID + "," + purchaseDate.format(formatter);
+
+}
+
+}
