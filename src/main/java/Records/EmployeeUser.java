c925917585b8b5b5d6ff643174ba4884820dd712
saif_eldeen <es.seifeldin.shady2023@alexu.edu.eg>
2025-10-18 13:11:19 +0300

    added Product and EmployeeUser classes

--- src/main/java/Records/EmployeeUser.java
+++ src/main/java/Records/EmployeeUser.java
@@ -2,15 +2,31 @@
 package Records;
 
 public class EmployeeUser implements RecordsInterface {
+
+    private String employeeId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    
+        public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber){
+            this.employeeId = employeeId;
+            this.name = name;
+            this.email = email;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+        }
+
     @Override
-    public String lineRepresentation(){return null;}
+    public String lineRepresentation(){
+        String lineRep = this.employeeId + "," + this.name + "," + this.email + "," + this.address + "," + this.phoneNumber;
+        return lineRep;
+    }
 
     @Override
     public String getSearchKey() {
-        return "";
+        return this.employeeId;
     }
 
-    public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber){
-
-    }
+    
 }
--- src/main/java/Records/Product.java
+++ src/main/java/Records/Product.java
@@ -2,15 +2,50 @@
 package Records;
 
 public class Product implements RecordsInterface {
+    
+    private String productID;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+    private int quantity;
+    private float price;
+
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
     @Override
-    public String lineRepresentation(){return null;}
+    public String lineRepresentation(){
+        String line = this.manufacturerName+","+
+                        this.supplierName+","+
+                        this.productID+","+
+                        this.productName+","+
+                        this.quantity+","+
+                        this.price; 
+        return line;
+    }
 
     @Override
     public String getSearchKey() {
-        return "";
+        return this.productID;
     }
 
-    ;
-    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if(quantity >= 0) {
+            this.quantity = quantity;
+        }
+        else {
+            System.out.println("Quantity cannot be negative.");
+            this.quantity = 0;
+        }
     }
 }
